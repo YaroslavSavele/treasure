d601fbd51ec325d121f2a4565c52007a0c464973
Jaroslav <hero34@mail.ru>
2022-05-06 16:04:05 +0500

    first commit

---
+++ models/Article.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $description
+ * @property string|null $content
+ * @property string|null $date
+ * @property string|null $image
+ * @property int|null $wiewed
+ * @property int|null $user_id
+ * @property int|null $status
+ * @property int|null $category_id
+ *
+ * @property ArticleTag[] $articleTags
+ * @property Comment[] $comments
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['description', 'content'], 'string'],
+            [['date'], 'date', 'format' => 'php:Y-m-d'],
+            [['date'], 'default', 'value' => date('Y-m-d')],
+            [['wiewed', 'user_id', 'status', 'category_id'], 'integer'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'date' => 'Date',
+            'image' => 'Image',
+            'wiewed' => 'Wiewed',
+            'user_id' => 'User ID',
+            'status' => 'Status',
+            'category_id' => 'Category ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[ArticleTags]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticleTags()
+    {
+        return $this->hasMany(ArticleTag::className(), ['article_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Comments]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::className(), ['article_id' => 'id']);
+    }
+
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
+    }
+
+    public function saveImage($filename)
+    {
+        $this->image = $filename;
+        //var_dump($this->image);die;
+        return $this->save(false);
+    }
+
+    public function getImage()
+    {
+        return ($this->image) ? '/uploads/' . $this->image : '/no-image.png';
+    }
+
+    public function deleteImage()
+    {
+        $imageUploadModel = new ImageUpload();
+        $imageUploadModel->deleteCurrentImage($this->image);
+    }
+
+    public function beforeDelete() 
+    {
+        $this->deleteImage();
+        return parent::beforeDelete();
+    }
+
+    public function saveCategory($category_id)
+    {
+        $category = Category::findOne($category_id);
+        if ($category !=null) {
+
+            $this->link('category', $category);
+            return true;
+        }
+        
+    } 
+}
